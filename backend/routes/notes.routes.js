321be1ea0cc9ae907ef08d0ef50523a26b106933
Dimpfe <dimasprasetyo7696@gmail.com>
2024-08-21 05:13:54 +0700

    feat<>: initiate controller

--- backend/routes/notes.routes.js
+++ backend/routes/notes.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { createNote, getNotes } from "../controller/notes.controller.js";
 
 const router = express.Router();
 
-router.get('/show');
-router.post('/add');
-router.post('/edit');
-router.delete('/delete');
+router.post('/add', createNote);
+router.get('/show', getNotes);
+router.get('/show/:id');
+router.post('/edit/:id');
+router.delete('/delete/:id');
 
 export default router;
