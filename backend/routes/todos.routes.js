972ea42abfcc7f7aaa3365db14f08084a2bbc02e
Dimpfe <dimasprasetyo7696@gmail.com>
2024-08-21 05:14:14 +0700

    feat<>: initiate controller

--- backend/routes/todos.routes.js
+++ backend/routes/todos.routes.js
@@ -1,13 +1,12 @@
 import express from "express";
 
 
-
 const router = express.Router();
 
 
 router.get('/show');
 router.post('/add');
-router.post('/edit');
-router.delete('/delete');
+router.post('/edit/:id');
+router.delete('/delete/:id');
 
 export default router;
