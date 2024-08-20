e8b1f14be9d01ec07ae3d0dd5f3fed6866bef2e5
Dimpfe <dimasprasetyo7696@gmail.com>
2024-08-21 05:11:25 +0700

    feat, fix<>: add use tag routes and fix import problem

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
-import dbConnection from './db/dbConnection';
-import notesRoutes from './routes/notes.routes';
-import todosRoutes from './routes/todos.routes';
+import dbConnection from "./db/dbConnection.js";
+import notesRoutes from "./routes/notes.routes.js";
+import todosRoutes from "./routes/todos.routes.js";
+import tagRoutes from "./routes/tag.routes.js";
 
 dotenv.config();
 
 const app = express();
 const HOST = 'localhost';
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/todos', todosRoutes);
 app.use('/api/notes', notesRoutes);
+app.use('/api/tag', tagRoutes);
 
 app.listen(PORT, () => {
-   console.log(`Server listening on ${HOST}:${PORT}`);
+   console.log(`Server listen on ${HOST}:${PORT}`);
    dbConnection();
 })
