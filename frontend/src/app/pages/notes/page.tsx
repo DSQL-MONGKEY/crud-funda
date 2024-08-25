bcb6f2a44f6080dba8328fc8a3f5b8286d8de079
Dimpfe <dimasprasetyo7696@gmail.com>
2024-08-25 21:29:10 +0700

    feat<>: create page notes

---
+++ frontend/src/app/pages/notes/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus voluptatum esse culpa officiis rerum commodi dolores eveniet sunt eos animi doloremque, non architecto minima a dolorum, repellat rem ad quae sequi cumque adipisci reprehenderit ab exercitationem sed? Quidem modi neque blanditiis reprehenderit eligendi vitae ipsam ea architecto incidunt. Aperiam quasi ab deleniti illo quas expedita itaque sed blanditiis. Doloribus praesentium tempora quaerat assumenda maxime nihil accusamus consequuntur sequi? Nisi ut eveniet molestiae sint molestias, odio fuga. Ut mollitia nisi et facilis. Reiciendis officia neque ut vitae iusto repellat maiores omnis illo, optio similique ad dolorum amet? Tempora id tempore blanditiis!
+    </main>
+  );
+}
