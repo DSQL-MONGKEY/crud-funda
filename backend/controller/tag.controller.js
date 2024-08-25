e9d61dd07513db9bfa71c52473b278daf49210a0
Dimpfe <dimasprasetyo7696@gmail.com>
2024-08-25 21:26:40 +0700

    feat<>: create tag handler

--- backend/controller/tag.controller.js
+++ backend/controller/tag.controller.js
@@ -12,9 +12,52 @@ export const createTag = async(req, res) => {
 
       res.status(201).json(newTag)
    } catch(err) {
-      console.log(`Error in createTag controller: ${err.message}`);
       res.status(500).json({ 
          error: `Internal server error occured: ${err.message}` 
       })
+      console.log(`Error in createTag controller: ${err}`);
+   }
+}
+
+export const getTags = async(req, res) => {
+   try {
+      const tags = await Tag.find()
+         .sort({ createdAt: -1 });
+
+      if(tags.length === 0) {
+         res.status(200).json([]);
+      }
+
+      res.status(200).json(tags);
+   } catch(err) {
+      res.status(500).json({ 
+         error: `Internal server error occured: ${err.message}` 
+      })
+      console.log(`Error in getTags controller: ${err}`);
+   }
+}
+
+export const deleteTag = async(req, res) => {
+   const { id } = req.params;
+
+   try {
+      const tag = await Tag.findById(id);
+
+      if(!tag) {
+         res.status(404).json({
+            error: 'Tag not found'
+         });
+      }
+
+      await Tag.findByIdAndDelete(id);
+
+      res.status(200).json({
+         message: 'Tag deleted successfully'
+      })
+   } catch(err) {
+      res.status(500).json({ 
+         error: `Internal server error occured: ${err.message}` 
+      })
+      console.log(`Error in deleteTag controller: ${err}`);
    }
 }
