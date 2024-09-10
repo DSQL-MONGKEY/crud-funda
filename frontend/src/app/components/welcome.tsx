ee143326ad63b489782912d38a3041306af1a873
Dimpfe <dimasprasetyo7696@gmail.com>
2024-09-10 13:59:52 +0700

    initial create

---
+++ frontend/src/app/components/welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = () => {
+   return (
+      <div>
+         <span>Hello user!</span>
+         <p>Do you think you dont have a plan?Do it</p>
+      </div>
+   )
+}
+
+export default Welcome
